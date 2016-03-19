9f7fc02b40bca858a27ab425bc763fc60f7e90cd
mooromets <sergey.sambor@gmail.com>
2016-03-19 18:49:01 +0200

    EPL table script updated

--- script/EPLTable.js
+++ script/EPLTable.js
@@ -3,8 +3,7 @@ cursor = db.games.aggregate
 			( [ 
 				// unwinding every team is one row now
 				{ $unwind: { path: "$teams", includeArrayIndex: "HomeAway" } }
-				//match EPL and home games only
-				,{ $match: { competition : "E0" , HomeAway : 0 } }
+				//match EPL and home games only 				,{ $match: { competition : "E0" , HomeAway : 0 } }
 /*				
 				,{ $project :
 					{
@@ -18,14 +17,38 @@ cursor = db.games.aggregate
 */				
 				,{ $group: 
 					{ 
+//						_id :  {team: "$teams", host: "$HomeAway"},
 						_id :  "$teams",
 						G : { $sum : 1 },
-						W : { $sum : { $cond: { if: { $eq : ['$matchResult.fullTimeResult', 'H']}, then: 1, else: 0 } } },
+						W : { $sum : { $cond: { if: { $or : [ 
+															{ $and : [ { $eq : ['$matchResult.fullTimeResult', 'H'] }, { $eq : ['$HomeAway', 0] } ] }, 
+															{ $and : [ { $eq : ['$matchResult.fullTimeResult', 'A'] }, { $eq : ['$HomeAway', 1] } ] }
+															]
+															}, then: 1, else: 0 } } },
 						D : { $sum : { $cond: { if: { $eq : ['$matchResult.fullTimeResult', 'D']}, then: 1, else: 0 } } },
-						L : { $sum : { $cond: { if: { $eq : ['$matchResult.fullTimeResult', 'A']}, then: 1, else: 0 } } },
-						Points : {$sum : {$add: [ {$multiply : [ { $cond: { if: { $eq : ['$matchResult.fullTimeResult', 'H']}, then: 1, else: 0 } }, 3 ] }, { $cond: { if: { $eq : ['$matchResult.fullTimeResult', 'D']}, then: 1, else: 0 } } ]}}
+						L : { $sum : { $cond: { if: { $or : [ 
+															{ $and : [ { $eq : ['$matchResult.fullTimeResult', 'A'] }, { $eq : ['$HomeAway', 0] } ] }, 
+															{ $and : [ { $eq : ['$matchResult.fullTimeResult', 'H'] }, { $eq : ['$HomeAway', 1] } ] }
+															]
+															}, then: 1, else: 0 } } },
+						GS : { $sum : { $cond: ["$HomeAway", "$matchResult.fullTimeScore.1" , "$matchResult.fullTimeScore.0"] } },
+						GC : { $sum : { $cond: ["$HomeAway", "$matchResult.fullTimeScore.0" , "$matchResult.fullTimeScore.1"] } } 
 					}
 				}
+				,{ $project :
+					{
+//						_id : {team : "$_id.team", side: {$cond: ["$HomeAway", "Aw", "Ho"] } },
+						G : 1,
+						W : 1,
+						D : 1,
+						L : 1,
+						GS : 1,
+						GC : 1,
+						GD : {$subtract: [ "$GS", "$GC" ]},
+						Points : {$sum : {$add: [ {$multiply : [ "$W", 3 ] }, "$D" ]}}
+					}
+				}
+				, { $sort : { Points : -1, W : -1, _id : 1 } }
 					
 			] )
 
