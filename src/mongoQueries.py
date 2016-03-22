dacbe836ba2e60307256cafca8d7304eff67a46e
mooromets <sergey.sambor@gmail.com>
2016-03-22 20:50:14 +0200

    league form table calculation added

--- src/mongoQueries.py
+++ src/mongoQueries.py
@@ -18,8 +18,6 @@ condAnd = lambda gr, idx: { "$and" : [ { "$eq": [ '$matchResult.fullTimeResult',
 def sortEPL():
 	return { "$sort" : { "Pts" : -1, "GD" : -1, "GS" : -1, "W" : -1 } }
 
-	
-			
 LeagueTablePipeine = [
 	matchLeagueTerm("E0", datetime(2015, 8, 1), datetime(2016, 6, 1))
 	,{ "$unwind": { "path": "$teams", "includeArrayIndex": "Ground" } }
@@ -74,3 +72,44 @@ LeagueTablePipeine = [
 					}
 	}	
 ]
+
+def groupResultsAndGoals(isHome, ground="Ground"):
+	checkResult = lambda res: { "$cond":{ "if": { "$eq": [ '$matchResult.fullTimeResult', res ] }, 
+										"then": 1, "else": 0 } }
+	return 	{ "$group":
+				{	
+					"_id":  { "club": "$teams", ground : "$"+ground },
+					"P":	{ "$sum": 1 },
+					"W":	{ "$sum": checkResult('H') if isHome else checkResult('A') },
+					"D":	{ "$sum": checkResult('D') },
+					"L": 	{ "$sum": checkResult('A') if isHome else checkResult('H') },
+					"F": 	{ "$sum": "$matchResult.FTHG" if isHome else "$matchResult.FTAG" } ,
+					"A": 	{ "$sum": "$matchResult.FTAG" if isHome else "$matchResult.FTHG" } 
+				}
+			}
+def projectTableView(ground="Ground"):
+	return { "$project":
+					{
+						"Club" : "$_id.club",
+						ground : { "$cond": ["$_id."+ground, 'A' , 'H'] },
+						"P" : 1,
+						"W" : 1,
+						"D" : 1,
+						"L" : 1,
+						"F" : 1,
+						"A" : 1,
+						"GD" : { "$subtract": [ "$GS", "$GC" ] },
+						"Pts" : { "$sum" : { "$add": [ { "$multiply" : [ "$W", 3 ] }, "$D" ] } } 
+					}
+			}
+
+#a if test else b
+
+def pipeLeagueTable(league, start, end, isHome):
+	return	[
+				matchLeagueTerm(league, start, end),
+				{ "$unwind": { "path": "$teams", "includeArrayIndex": "Ground" } },
+				{ "$match": {"Ground": 0 if isHome else 1 } },
+				groupResultsAndGoals(True),
+				projectTableView()
+			]
--- src/test.py
+++ src/test.py
@@ -2,21 +2,60 @@ import mongoConnect
 from datetime import datetime
 
 import mongoQueries
+
 #open MongoDB connection
-
-
 hm = mongoConnect.HandiMongo("games")
 
-pipeline = [
-	mongoQueries.matchLeagueTerm("E0", datetime(2015, 8, 1), datetime(2016, 6, 1)),
-	{ "$unwind" : { "path": "$teams", "includeArrayIndex": "HomeAway" } },
-	{ "$group" : { "_id" : "$teams" } }
+# prepare competition context
+
+PL = "E0"
+startPL = datetime(2015, 8, 1)
+endPL = datetime(2016, 6, 1)
+collLeagueTable = "leagueTable"
+hm.db[collLeagueTable].drop()
+collLeagueForm = "leagueForm"
+hm.db[collLeagueForm].drop()
+
+#home games
+pipePLHome = mongoQueries.pipeLeagueTable(PL, startPL, endPL, True)
+pipePLHome.append( { "$out" : collLeagueTable } )
+hm.collTest.aggregate(pipePLHome) 
+#away games
+pipePLHome = mongoQueries.pipeLeagueTable(PL, startPL, endPL, False)
+for doc in list(hm.collTest.aggregate(pipePLHome)):	
+	hm.db[collLeagueTable].insert_one(doc)
+
+# major loop on all teams
+pipeAllTeams = [
+	{ "$unwind": "$teams" },
+	mongoQueries.matchLeagueTerm(PL, startPL, endPL),
+	{ "$group": { "_id": "$teams" } }
 ]
 
-newColPipeline = list(mongoQueries.LeagueTablePipeine)
-newColPipeline.append( { "$out" : "leagueTable" } )
+for team in list(hm.collTest.aggregate(pipeAllTeams)):
+	print (team['_id'])
+	
+	#refactor this shit
+	
+	matchTeamLimit = mongoQueries.matchLeagueTerm(PL, startPL, endPL)
+	matchTeamLimit["$match"]["teams.0"] = team['_id']
 
-for doc in list(hm.collTest.aggregate(newColPipeline)):
-	print (doc)
+	pipeStats = lambda isHome: [
+		matchTeamLimit,
+		{ "$sort": { "gameDate" : -1 } },
+		{ "$limit" : 6 },
+		{ "$unwind": { "path": "$teams", "includeArrayIndex": "Ground" } },
+		{ "$match": {"Ground": 0 if isHome else 1 } },
+		mongoQueries.groupResultsAndGoals(isHome),
+		mongoQueries.projectTableView()
+	]
+	
+	for doc in list(hm.collTest.aggregate(pipeStats(True))):
+		hm.db[collLeagueForm].insert_one(doc)
 
-
+	matchTeamLimit = mongoQueries.matchLeagueTerm(PL, startPL, endPL)
+	matchTeamLimit["$match"]["teams.1"] = team['_id']
+		
+	for doc in list(hm.collTest.aggregate(pipeStats(False))):
+		hm.db[collLeagueForm].insert_one(doc)		
+	
